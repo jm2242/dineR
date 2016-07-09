06fccedbc8b98786950144326863580fc97339db
Logan Nathanson <lnathanson@wayfair.com>
2016-07-09 01:48:53 -0400

    icon change

--- client/components/AppBody.jsx
+++ client/components/AppBody.jsx
@@ -5,7 +5,7 @@ AppBody = React.createClass({
         <div className="bar bar-header bar-light">
           <ReactRouter.Link className="button button-icon icon ion-gear-a" to={"/settings"}></ReactRouter.Link>
           <ReactRouter.Link className="h1 title" to={"/"}>dineR</ReactRouter.Link>
-          <ReactRouter.Link className="button button-icon icon ion-gear-a" to={"/savedItems"}></ReactRouter.Link>
+          <ReactRouter.Link className="button button-icon icon ion-navicon" to={"/savedItems"}></ReactRouter.Link>
 
         </div>
 
