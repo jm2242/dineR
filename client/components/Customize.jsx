dcb30e4c1c6522a5c33645adba2b06253dd2559e
KevinMcK <kevin.mck99@gmail.com>
2016-07-09 13:24:40 -0400

    broken css

--- client/components/Customize.jsx
+++ client/components/Customize.jsx
@@ -2,7 +2,7 @@ Customize = React.createClass({
   mixins: [ReactMeteorData],
   getMeteorData() {
     let handle = Meteor.subscribe("myData")
-    let data = MyData.find({affirmative: true}).fetch()
+    let data = meals.find({affirmative: true}).fetch()
     return {
       loading: !handle.ready(),
       users: data
@@ -12,18 +12,18 @@ Customize = React.createClass({
     Meteor.call('reset')
   },
   render() {
-    if (this.data.loading) {
-      return <h1>Loading</h1>
-    }
-    let list = this.data.users.map((user) => {
-      return (
-        <div className="item item-avatar" key={user._id}>
-          <img src={user.image}></img>
-          <h2>{user.name}</h2>
-          <p>{user.details}</p>
-        </div>
-      )
-    })
+  //   if (this.data.loading) {
+  //     return <h1>Loading</h1>
+  //   }
+  //   let list = this.data.users.map((user) => {
+  //     return (
+  //       <div className="item item-avatar" key={user._id}>
+  //         <img src={user.image}></img>
+  //         <h2>{user.name}</h2>
+  //         <p>{user.details}</p>
+  //       </div>
+  //     )
+  //   })
     return (
       <div className="list">
         <div className="item item-divider">
@@ -33,10 +33,11 @@ Customize = React.createClass({
          <p>Lettuce<input type="checkbox" value="Hello!" defaultChecked/></p>
          <p>Lettuce<input type="checkbox" value="Hello!" defaultChecked/></p>
          <p>Lettuce<input type="checkbox" value="Hello!" defaultChecked/></p>
-        <div className="item item-divider">
-          <span className="database-reset-button">Order</span>
+        <div className="bar bar-footer bar-assertive">
+          <h1 class="title">Order</h1>
         </div>
       </div>
     )
   }
+  //mealOptions: {"Ingredient" : true, "Ingredient2" : false}
 });
