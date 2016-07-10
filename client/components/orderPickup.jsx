dace4bd339c7063fa3a4abb2437f75b4ac206630
Logan Nathanson <lnathanson@wayfair.com>
2016-07-09 21:10:36 -0400

    Fixed ending landing pages

--- client/components/orderPickup.jsx
+++ client/components/orderPickup.jsx
@@ -1,9 +1,23 @@
 orderPickup = React.createClass({
   render() {
+    let swipedMeal = meals.findOne({_id: this.props.params.mealId});
     return (
     	<div className="">
     		<h3>Your Order has been placed!</h3>
-    		<h5>It will be ready in 6 minutes for pick up!</h5>
-    	</div>
+    		<h5>It will be ready in 6 minutes!</h5>
+    		<div className="card container">
+	          <div className="item item-body column">
+	            <img className="full-image column" src={swipedMeal.image} />
+	          </div>
+	          <div className="item">
+	            <h2>{swipedMeal.name}</h2>
+	            <h3>{swipedMeal.restaurant}</h3>
+	            <p>Order Total: {swipedMeal.price}</p>
+	          </div>
+	          <div className="item word-will-wrap">
+	            <p>{swipedMeal.details}</p>
+	          </div>
+	        </div>
+	    </div>
     )
 }})
