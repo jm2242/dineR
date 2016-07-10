8c7153bcbd4e7492a8354ccf73270dd2dca074d4
ats74 <ats74@cornell.edu>
2016-07-09 21:11:51 -0400

    email stuff

--- client/components/orderMethod.jsx
+++ client/components/orderMethod.jsx
@@ -1,3 +1,4 @@
+
 orderMethod = React.createClass({
   mixins: [ReactMeteorData],
   getMeteorData() {
@@ -8,6 +9,13 @@ orderMethod = React.createClass({
       users: data
     }
   },
+sendConfirmation(){
+	Meteor.call('sendEmail',
+	            'atsy314@gmail.com',
+	            'atsy314@gmail.com',
+	            'Hello from Meteor!',
+	            'This is a test of Email.send.');
+	},
   reset() {
     Meteor.call('reset')
   },
@@ -15,8 +23,8 @@ orderMethod = React.createClass({
 	  return (
 	  	<div>
 		  	<div className= "buttons">
-				<ReactRouter.Link className="button button-block button-assertive" to={"/orderPlaced/" + this.props.params.mealId}>Delivery</ReactRouter.Link>
-				<ReactRouter.Link className="button button-block button-calm" to={"/orderPickup/" + this.props.params.mealId}>Pick Up</ReactRouter.Link>
+				<ReactRouter.Link clickHandler={this.sendConfirmation} className="button button-block button-assertive" to={"/orderPlaced/" + this.props.params.mealId}>Delivery</ReactRouter.Link>
+				<ReactRouter.Link clickHandler={this.sendConfirmation} className="button button-block button-calm" to={"/orderPickup/" + this.props.params.mealId}>Pick Up</ReactRouter.Link>
 			</div>
 		</div>
 	  	)
--- server/server.jsx
+++ server/server.jsx
@@ -1,6 +1,22 @@
 Meteor.publish("meals", function() {
   return meals.find()
 })
+Meteor.methods({
+  sendEmail: function (to, from, subject, text) {
+    check([to, from, subject, text], [String]);
+
+    // Let other method calls from the same client start running,
+    // without waiting for the email sending to complete.
+    this.unblock();
+
+    Email.send({
+      to: to,
+      from: from,
+      subject: subject,
+      text: text
+    });
+  }
+})
 populate = function() {
   
   meals.insert({
